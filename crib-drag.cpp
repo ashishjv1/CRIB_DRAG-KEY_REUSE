e9f7cc9ee5b0bbf19482e6cfa60f8ba282f5430c
murdockbleak <ashish.jhav1@hotmail.com>
2020-05-14 19:24:28 +0300

    modified:   crib-drag.cpp

--- crib-drag.cpp
+++ crib-drag.cpp
@@ -38,51 +38,97 @@ void string2hexString(char* input, char* output)
 }
 
 
-string hexToBinary(string s) {
-	string tmp;
-	for(int i = 0; i < s.size(); i++) {
-	    switch(toupper(s[i])) {
-	        case '0': tmp += "0000";
-	        case '1': tmp += "0001";
-	        case '2': tmp += "0010";
-	        case '3': tmp += "0011";
-	        case '4': tmp += "0100";
-	        case '5': tmp += "0101";
-	        case '6': tmp += "0110";
-	        case '7': tmp += "0111";
-	        case '8': tmp += "1000";
-	        case '9': tmp += "1001";
-	        case 'A': tmp += "1010";
-	        case 'B': tmp += "1011";
-	        case 'C': tmp += "1100";
-	        case 'D': tmp += "1101";
-	        case 'E': tmp += "1110";
-	        default: tmp += "1111";
-	    }
-	}
-	return tmp;
-}
+string HexToBin(string hexdec) 
+{ 
+    long int i = 0; 
+  	string tmp;
+    while (hexdec[i]) { 
+  
+        switch (hexdec[i]) { 
+        case '0': 
+            tmp += "0000"; 
+            break; 
+        case '1': 
+            tmp += "0001"; 
+            break; 
+        case '2': 
+            tmp += "0010"; 
+            break; 
+        case '3': 
+            tmp += "0011"; 
+            break; 
+        case '4': 
+            tmp += "0100"; 
+            break; 
+        case '5': 
+            tmp += "0101"; 
+            break; 
+        case '6': 
+            tmp += "0110"; 
+            break; 
+        case '7': 
+            tmp += "0111"; 
+            break; 
+        case '8': 
+            tmp += "1000"; 
+            break; 
+        case '9': 
+            tmp += "1001"; 
+            break; 
+        case 'A': 
+        case 'a': 
+            tmp += "1010"; 
+            break; 
+        case 'B': 
+        case 'b': 
+            tmp += "1011"; 
+            break; 
+        case 'C': 
+        case 'c': 
+            tmp += "1100"; 
+            break; 
+        case 'D': 
+        case 'd': 
+            tmp += "1101"; 
+            break; 
+        case 'E': 
+        case 'e': 
+            tmp += "1110"; 
+            break; 
+        case 'F': 
+        case 'f': 
+            tmp += "1111"; 
+            break; 
+        default: 
+            cout << "\nInvalid hexadecimal digit ";
+            
+            cout <<tmp[i]; 
+        } 
+        i++; 
+    } 
+return tmp;
+} 
 
 void xorstring(string input_string, string random_string)
 {
 	//char test2 = random_string;
 	int n = input_string.length();
-	string binary1 = hexToBinary(input_string);
-	string binary2 = hexToBinary(random_string);
-	int len = binary1.size();
-	char final_key[len]; 
-	cout<<"Binary-String:" << binary1;
+	string binary1 = HexToBin(input_string);
+	string binary2 = HexToBin(random_string);
+	int len = binary2.size();
+	//cout << "length is"<<len;
+	char final_key[len];              //need to check size
+	cout<<"Binary-String: " << binary1;
 	cout<< endl;
-	cout<<"Binary-Random:" << binary2;
+	cout<<"Binary-Random: " << binary2;
 	cout << endl;
 	for(int i=0; i < len; i++)
 	{
-
-	//	final_key[i] = (binary1[i] ^ binary2[i])+'0';
-		final_key[i] = (binary1[i] ^ binary2[i]); 
-		cout << final_key ;
+		final_key[i] = ((binary1[i] - '0') ^ (binary2[i] - '0')) + '0';
+	//	final_key[i] = (binary1[i] ^ binary2[i]); 
+	//	cout << final_key ;
 	}
-	//cout << "Final Key is: " << final_key << endl;
+	cout<<"Encrypted data:"<< final_key<<endl;
 	
 } 
 
